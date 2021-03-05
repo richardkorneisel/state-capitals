498ae66c0db3ca02645287df8d6a39a9f23a6f2e
Rich Korneisel <korneisel@cfu.net>
2021-03-04 19:06:15 -0600

    add correc, subtract incorrect

--- lib/script.py
+++ lib/script.py
@@ -2,9 +2,36 @@ from capitals import states
 import random
 
 def name_capital_game():
-    random.shuffle(states)
-    print(states[0]['name'])
-    print(states[0]['capital'])
+    play_game=input('Do you want to play the guess your capitals game? y or n')
+    
+    score_right=0
+    score_wrong=0
+    incorrect=0
+    
+    round = score_right+score_wrong
+    
+    if play_game=='y':
+        print ('Type the state capital for the state displayed ')
+        random.shuffle(states)
+        
+        print(states[0]['name'])
+        answer=input()
+        if answer==states[0]['capital']:
+            print('Correct!! Great Job.')
+            score_right = score_right+1
+            print(score_right)
+            states.pop(0)
+        else:
+            print('Sorry, wrong answer')
+            score_wrong = score_wrong+1
+            print(score_wrong)
+            states.pop(0)
+        
+    else:
+        print('Goodbye, maybe we can play again')
+    
+    print('round')
+    
 
 name_capital_game()
 
