6e58c0ba27c8d1079c21a8906e427c4ff92f9d05
Rich Korneisel <korneisel@cfu.net>
2021-03-04 18:21:21 -0600

    finally pulled the state and captial, worth a commit

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,12 @@
 from capitals import states
+import random
 
-print(states)
+def name_capital_game():
+    random.shuffle(states)
+    print(states[0]['name'])
+    print(states[0]['capital'])
+
+name_capital_game()
+
+#for i in states:
+#print(states)
